3d001196ad3dcb190030e68c5dc6ba3817701db1
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-06 12:01:53 +0600

    sum with while loop

--- sum-of-odd-numbers.js
+++ sum-of-odd-numbers.js
@@ -37,5 +37,27 @@ Subtask-2:
 Display sum of all the even numbers from 206 to 311.
 
  */
+let sum2 = 0;
+for(let i = 206; i<=311; i++){
+    if(i % 2 ===0){
+        console.log(i);
+        sum2 = sum2 + i;
+        console.log(sum2);
+    }
+    console.log('total sum of the all even numbers is',sum2);
+}
+// using while loop
+let nc = 206;
+let sum_even = 0;
+while(nc<=311){
+    console.log(nc);
+    if(nc %2===0){
+        console.log(nc);
+        sum_even = sum_even + nc;
+        console.log(sum_even);
+    }
+    nc++;
+}
+console.log('total some of all even numbers is', sum_even);
 
 /*programming hero*/
